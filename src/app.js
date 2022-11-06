f17a4b3f74fd5fa64380c6d101a5736d4d701e6d
Lhais <lhaisferreirada@gmail.com>
2022-11-06 17:27:47 -0300

    feat: adição de conexão ao banco de dados mongoDB

--- src/app.js
+++ src/app.js
@@ -6,10 +6,23 @@ import * as dotenv from "dotenv";
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000;
+const environment = process.env.ENVIRONMENT || "DEV"
+const user = process.env.USER_DB
+const password = process.env.PASSWORD
+const database = process.env.DATABASE
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(port, ()=>{
-    console.log(`http://localhost:${port}`);
-});
+try {
+    await mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.vqfs4id.mongodb.net/${database}?retryWrites=true&w=majority`)
+    console.log("conectado ao mongo")
+    app.listen(port, () => {
+        if(environment === "PROD")  {
+            console.log("Ambiente de produção.")
+        }
+        console.log(`http://localhost:${port}`)
+    })
+} catch (error) {
+    console.log(error.message)
+}
